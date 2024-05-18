312cc1cdb92cee711b6d99521285cf3216c25675
mfaruqi35 <muhammad.faruqi35@gmail.com>
2024-05-18 22:46:22 +0700

    kerangka

---
+++ header.h
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+typedef struct {
+    double skor;
+    char kata[64];
+    int panjangKata;
+} Data;
+
+typedef struct {
+    double skor;
+    char kata[64];
+    int panjangKata;
+} Final;
+
+void writeBinaryFile (Data datas[], int );
