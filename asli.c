87662c39454acb252283657f5c3a47b7c2eacf47
Affan Suhendar <affansuhendar1@gmail.com>
2024-05-19 21:13:33 +0700

    Update

---
+++ asli.c
@@ -0,0 +1,114 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include "header.h"
+
+
+
+int main(){
+
+    // ---- Affan var
+    int Opsi_1 = 0;
+    int Opsi_2 = 0;
+    // ----
+    int option;
+    int is_continue = 1;
+    int option1_selected = 0;
+    int line = 0;
+    int length = 0;
+    int kata = 0;
+    FILE *ifp;
+    char filename[20];
+    //Data contoh[kata];
+  
+    do{
+        int Berhasil_1 = 1;
+        Clear_System();
+        Header();
+        printf("    Sebelum Memulai, Silahkan input nama file (txt)\n\n");
+        printf("      [1] Input file text\n");
+        printf("      [2] exit\n\n");
+        printf("    Pilihan Anda (1/2) : ");
+        scanf("%d",&Opsi_1);
+        ignoreInputBuffer();
+        
+        do{
+            if (Opsi_1 == 1){
+                Opsi_1 = 0; //Reset
+                Clear_System();
+                Header();
+                printf("    Masukkan nama file input (txt): ");
+                scanf("%s", filename);
+                ifp = fopen(filename, "r");
+                if(ifp == NULL){
+                    printf("    File ridak ditemukan, Please Wait ...\n");
+                    Pause();
+                    Berhasil_1 = 0;
+                }  
+            } 
+            fclose(ifp);
+        }while(Berhasil_1 != 0); 
+
+    /*
+        int Berhasil = 1;
+        Clear_System();
+        Header();
+        printf("Masukkan nama file input (txt): ");
+        scanf("%s", filename);
+        ifp = fopen(filename, "r");
+
+            if(ifp == NULL){
+                printf("Error, tidak dapat membuka file...\n");
+                Berhasil = 0;
+                exit(1);
+            }   
+        fclose(ifp);
+
+
+        system("cls");
+        printf("Selamat Datang di Big challenge\n\n");
+        printf("(1) Masukan ke binary file\n");
+        printf("(2) Baca dari binary file\n");
+        printf("(3) Keluar\n");
+        printf("Masukkan pilihan anda: ");
+        scanf("%d", &option);
+        ignoreInputBuffer();
+    
+        if(option == 1){
+            printf("program write to file binary\n");
+            writeBinaryFile(contoh, kata);
+            getEnterKey();
+            goto label_input;
+        }
+        else if(option == 2){
+                if(option1_selected){
+                    printf("program read file binary\n");
+                    //read binary and print to terminal
+                    getEnterKey();
+                    goto label_input;
+                }
+                else {
+                    printf("Pilih opsi satu terlebih dahulu");
+                    getEnterKey();
+                    goto label_input; 
+                }
+        //     
+        }
+        else if(option == 3){
+            printf("Program selesai, Sampai jumpa");
+        }
+        else {
+            printf("input tidak valid, silahkan masukkan input ulang\n");
+            getEnterKey();
+            goto label_input;
+        }
+
+        */
+       if (Opsi_1 == 2){
+        printf("    Terima Kasih :)\n");
+       }
+
+    }while(Opsi_1 !=2);
+    return 0;
+}
--- main.c
+++ main.c
@@ -4,108 +4,89 @@
 #include <string.h>
 #include "header.h"
 
+int main() {
 
-
-int main(){
-
-    // ---- Affan var
     int Opsi_1 = 0;
     int Opsi_2 = 0;
-    // ----
-    int option;
-    int is_continue = 1;
-    int option1_selected = 0;
-    int line = 0;
-    int length = 0;
-    int kata = 0;
     FILE *ifp;
     char filename[20];
-    //Data contoh[kata];
-  
-    label_input:
-    do{
-        int Berhasi_1 = 1;
+
+    // Menu Utama
+    do {
         Clear_System();
         Header();
-        printf("    Sebelum Memulai, Silahkan input nama file (txt)\n\n");
+        printf("    Sebelum Memulai, Silahkan input nama file ( File.txt )\n\n");
         printf("      [1] Input file text\n");
         printf("      [2] exit\n\n");
         printf("    Pilihan Anda (1/2) : ");
-        scanf("%d",&Opsi_1);
+        scanf("%d", &Opsi_1);
         ignoreInputBuffer();
 
-        do{
-            if (Opsi_1 == 1){
-                Opsi_1 = 0; //Reset
-                Clear_System();
-                Header();
-                printf("    Masukkan nama file input (txt): ");
-                scanf("%s", filename);
-                ifp = fopen(filename, "r");
-                if(ifp == NULL){
-                    printf("    Error, tidak dapat menemukan file...\n");
+        if (Opsi_1 == 1) {
+
+            int Berhasil = 0; 
+            Clear_System();
+            Header();
+            printf("    Masukkan nama file input ( File.txt ) : ");
+            scanf("%s", filename);
+            ifp = fopen(filename, "r");
+            if (ifp == NULL) {
+                    printf("    File tidak ditemukan, Mohon tunggu ...\n");
                     Pause();
-                    Berhasi_1 = 0;
-                }  
-            } 
-            fclose(ifp);
-        }while(Berhasi_1 != 0); 
+                    continue;;
+                } else {
 
-    /*
-        int Berhasil = 1;
-        Clear_System();
-        Header();
-        printf("Masukkan nama file input (txt): ");
-        scanf("%s", filename);
-        ifp = fopen(filename, "r");
+        
+         //------------------- Menu kedua ----------------------------------------------------
+                    
+                        do {
+                            Clear_System();
+                            Header();
+                            printf("      ------------( Menu / Option )------------\n\n");
+                            printf("      [1] Masukkan Data Ke File Binary\n");
+                            printf("      [2] Tampilkan Data Dari Binary\n");
+                            printf("      [3] Kembali\n\n");
+                            printf("    Pilihan Anda (1-3) : ");
+                            scanf("%d", &Opsi_2);
+                            ignoreInputBuffer();
 
-            if(ifp == NULL){
-                printf("Error, tidak dapat membuka file...\n");
-                Berhasil = 0;
-                exit(1);
-            }   
-        fclose(ifp);
+                            switch(Opsi_2) {
 
+                            case 1:
+                                    Clear_System();
+                                    printf("    \nOpsi pertama\n");
+                                    Pause();
+                                    fclose(ifp);
+                                    break;
 
-        system("cls");
-        printf("Selamat Datang di Big challenge\n\n");
-        printf("(1) Masukan ke binary file\n");
-        printf("(2) Baca dari binary file\n");
-        printf("(3) Keluar\n");
-        printf("Masukkan pilihan anda: ");
-        scanf("%d", &option);
-        ignoreInputBuffer();
-    
-        if(option == 1){
-            printf("program write to file binary\n");
-            writeBinaryFile(contoh, kata);
-            getEnterKey();
-            goto label_input;
-        }
-        else if(option == 2){
-                if(option1_selected){
-                    printf("program read file binary\n");
-                    //read binary and print to terminal
-                    getEnterKey();
-                    goto label_input;
+                            case 2:
+                                    Clear_System();
+                                    printf("    \nOpsi Kedua\n");
+                                    Pause();
+                                    fclose(ifp);
+                                    break;
+                            case 3: 
+                                     break;
+                            default:
+                                    printf("\n    Mohon Input Dengan Benar\n");
+                                    Pause();
+                                    fclose(ifp);
+                                    break;
+                            }
+
+                        } while (Opsi_2 != 3); 
                 }
-                else {
-                    printf("Pilih opsi satu terlebih dahulu");
-                    getEnterKey();
-                    goto label_input; 
-                }
-        //     
-        }
-        else if(option == 3){
-            printf("Program selesai, Sampai jumpa");
-        }
-        else {
-            printf("input tidak valid, silahkan masukkan input ulang\n");
-            getEnterKey();
-            goto label_input;
-        }
+            
 
-        */
-    }while(Opsi_1 !=2);
+
+        } else if (Opsi_1 == 2) {
+            printf("\n    Terima Kasih :)\n");
+        } else {
+            printf("\nMohon Input Dengan Benar\n");
+            Pause();
+        }
+    } while (Opsi_1 != 2);
+
     return 0;
 }
+
